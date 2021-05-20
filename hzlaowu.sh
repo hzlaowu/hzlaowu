30206bf33b7a5d7ab798a3b5636c7c067da31a2c
AlexWu2022 <65534189+AlexWu2022@users.noreply.github.com>
2021-05-20 10:50:18 +0800

    Update hzlaowu.sh

--- hzlaowu.sh
+++ hzlaowu.sh
@@ -121,6 +121,17 @@ function Oracle_root_passwd(){
     sudo reboot
 }
 
+function linux5.11(){
+    cd /tmp
+    wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100_5.11.0-051100.202102142330_all.deb
+    wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb 
+    wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-image-unsigned-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
+    wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
+    sudo dpkg -i *.deb
+    reboot
+}
+
+
 #服务器功能调试
 #安装BBR加速
 function Linux-NetSpeed(){
@@ -306,6 +317,7 @@ function start_menu(){
     green "  5. Install_wget/curl"
     green "  6. Oracle_Firewall"
 	green "  7. Oracle_root_passwd"
+	green "  8. linux5.11"
     	
     blue  " =======服务器功能======================================= "
     green " 11. 安装BBR加速 "
@@ -343,6 +355,8 @@ function start_menu(){
 	     6 )  Oracle_Firewall
 	;;
 	     7 )  Oracle_root_passwd
+	;;
+	     8 )  linux5.11
 	;;
 	    11 )  Linux-NetSpeed 
 	;;
